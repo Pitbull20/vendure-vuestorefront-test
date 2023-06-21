13b6dfbf7a8b30a4a9fe2026f22ae0a7ffb4ad1e
ultra_pitbull <rosolovskiy2005@gmail.com>
2023-06-21 16:10:06 +0300

    first commit

---
+++ client/components/TopBar.vue
@@ -0,0 +1,23 @@
+<template></template>
+
+<script>
+import { SfButton, SfTopBar } from "@storefront-ui/vue";
+import LocaleSelector from "./LocaleSelector";
+
+export default {
+  components: {
+    SfTopBar,
+    SfButton,
+    LocaleSelector,
+  },
+};
+</script>
+<style lang="scss" scoped>
+.topbar {
+  position: relative;
+  z-index: 2;
+  &__button {
+    margin: 0 0 0 var(--spacer-xs);
+  }
+}
+</style>
